90847a83622873029234e43a1b25af96e4f22e7e
coder-T03 <abhishekofficial997@gmail.com>
2025-11-09 18:54:33 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict";  // treat all JS code as newer version
+
+//alert(3+3) // it will show error bcz we are using nodejs, not browser
+
+// code readable should be priority
+
+// DATA TYPES
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//**number => 2 to power 53 -1
+//bigint 
+
+//string => "" or ''
+//boolean => Yes/No
+
+// null => standalone value // we can assign null
+let tempt = null
+// undefined => not given value to variable
+
+// symbol => use to find uniqueness
+
+
+// Object
+
+console.log(typeof "hitesh")  // typeof  tells the datatype
+
+console.log(typeof null);  // output => object
+console.log(typeof undefined)  // output => undefined
+
+
